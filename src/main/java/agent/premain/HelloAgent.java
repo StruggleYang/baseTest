d90885ea726d9e324e1ebcb1ad45e613fa5f82b0
StruggleYang <1724555319@qq.com>
2023-01-05 14:54:02 +0800

    premain

---
+++ src/main/java/agent/premain/HelloAgent.java
@@ -0,0 +1,13 @@
+package agent.premain;
+
+/**
+ * @author with struy.
+ * Create by 2023/1/3 17:02
+ * email :yq1724555319@gmail.com
+ */
+
+public class HelloAgent {
+    public static void premain(String args) {
+        System.out.println("Hello Agent:  " + args);
+    }
+}
---
+++ src/main/java/agent/premain/HelloWorld.java
@@ -0,0 +1,13 @@
+package agent.premain;
+
+/**
+ * @author with struy.
+ * Create by 2023/1/3 17:01
+ * email :yq1724555319@gmail.com
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
